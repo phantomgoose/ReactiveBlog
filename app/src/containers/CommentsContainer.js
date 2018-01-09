2082d1d2fe927c92ba776f972418abbec7ccc0b9
phantomgoose <alex.txt@gmail.com>
2018-01-09 14:09:02 -0800

    move posts/comments components into relevant containers

--- app/src/containers/CommentsContainer.js
+++ app/src/containers/CommentsContainer.js
@@ -1,5 +1,20 @@
-import Comments from "../components/Comments/Comments";
+import React, { Component } from "react";
+import Comment from "../components/Comment/Comment";
 import { connect } from "react-redux";
+
+class Comments extends Component {
+  render() {
+    const filteredComments = this.props.filteredComments;
+    if (!filteredComments) {
+      return <p>Loading comments...</p>;
+    }
+    return filteredComments.map(comment => {
+      return (
+        <Comment key={comment.id} name={comment.name} body={comment.body} />
+      );
+    });
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   const allComments = state.comments;
--- app/src/containers/PostsContainer.js
+++ app/src/containers/PostsContainer.js
@@ -1,5 +1,27 @@
-import Posts from "../components/Posts/Posts";
 import { connect } from "react-redux";
+import React, { Component } from "react";
+import Post from "../components/Post/Post";
+
+class Posts extends Component {
+  render() {
+    const posts = this.props.posts;
+
+    if (!posts) {
+      return <p>Loading posts...</p>;
+    }
+    return posts.map(post => {
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          body={post.body}
+          userName={post.userName}
+          id={post.id}
+        />
+      );
+    });
+  }
+}
 
 const mapStateToProps = state => {
   const posts = state.posts;
